e6f5593a66934a5950a4072c92896f13dac06b04
Suraj Mishra <mishrasur7@gmail.com>
2021-09-23 23:36:03 +0300

    first oop

---
+++ Chapter_8/Chapter_8/ObjectsPizzaProgram.java
@@ -0,0 +1,14 @@
+package Chapter_8;
+
+public class ObjectsPizzaProgram {
+
+	public static void main(String[] args) {
+		Pizza pizza1 = new Pizza("Diavolo", 8.75);
+		Pizza pizza2 = new Pizza("Frutti al Mare", 9.00);
+		Pizza pizza3 = new Pizza("Alla Pollo", 7.50);
+		System.out.println(pizza1);
+		System.out.println(pizza2);
+		System.out.println(pizza3);
+	}
+
+}
---
+++ Chapter_8/Chapter_8/Pizza.java
@@ -0,0 +1,27 @@
+package Chapter_8;
+
+import java.text.DecimalFormat;
+
+public class Pizza {
+	private String name; 
+	private double price; 
+	
+	Pizza(String name, double price) {
+		this.name = name; 
+		this.price = price; 
+	}
+	
+	String getName() {
+		return name; 
+	}
+	double getPrice() {
+		return price; 
+	}
+	
+	public String toString() {
+		DecimalFormat twoDecimals = new DecimalFormat("0.00"); 
+		return name + " (" + twoDecimals.format(price).replace('.', ',') + " euros)"; 
+	}
+	
+
+}
