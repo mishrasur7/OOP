aa5b09f7252090eb04312713d9237889f7d02853
Suraj Mishra <mishrasur7@gmail.com>
2021-09-24 23:04:04 +0300

    third largest value found

--- Chapter_9/src/Chapter_9/SecondLargest.java
+++ Chapter_9/src/Chapter_9/SecondLargest.java
@@ -7,19 +7,25 @@ public class SecondLargest {
 		int[] nums = { 2, 6, 8, 32, 56, 12, 84 };
 		int max = nums[0];
 		int secondMax = nums[0];
+		int thirdMax = nums[0]; 
 
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] > max) {
+				thirdMax = secondMax; 
 				secondMax = max;
 				max = nums[i];
 			} else if (nums[i] > secondMax) {
+				thirdMax = secondMax; 
 				secondMax = nums[i];
+			} else if(nums[i] > thirdMax) {
+				thirdMax = nums[i]; 
 			}
 
 		}
 		
 		System.out.println("Max is " + max);
 		System.out.println("Second max is " + secondMax);
+		System.out.println("Third max is " + thirdMax);
 
 	}
 
